c719e2b530a54cf3f8a477fde44aecad74174f38
Jerry Gu <ncq9fn@virginia.edu>
2023-12-31 23:11:20 -0500

    added project descriptions

--- portfolio/src/components/ProjectList.tsx
+++ portfolio/src/components/ProjectList.tsx
@@ -1,19 +1,19 @@
 const ProjectList = [
     {
-      title: 'Project 1',
-      details: ['Detail 1', 'Detail 2', 'Detail 3'],
+      title: 'Student Course Review App',
+      details: ['Designed and developed a class review application as a final project for Software Development Essentials course', '•	Engineered a 3-layer architecture pattern with JavaFX as the presentation layer, Java for logic, and SQLite for the backend'],
     },
     {
-      title: 'Project 2',
-      details: ['Detail 1', 'Detail 2', 'Detail 3'],
+      title: 'ShadowBox Mouse Trainer ',
+      details: ['Conceived the original idea for a mouse coordination training game, identifying a need for such a tool', 'Self-taught Unity and C# scripting to bring the game idea to life, successfully bringing new game to public ', 'To play: https://jerrygu.itch.io/shadowbox'],
     },
     {
-      title: 'Project 3',
-      details: ['Detail 1', 'Detail 2', 'Detail 3'],
+      title: 'SplitBall',
+      details: ['Inherited single player game as a team of two to repurpose as a multiplayer offline competitive game within the Unity Engine and C#', 'To view repo: https://github.com/UVASGD/spring-2022-splitball'],
     },
     {
-      title: 'Project 4',
-      details: ['Detail 1', 'Detail 2', 'Detail 3'],
+      title: 'Personal Portfolio Website',
+      details: ['Constructed a personal portfolio website using React + TypeScript, and designed using Tailwind CSS', 'Incorporated an email contact form using Formspree, providing direct communication with visitors', 'To view website: Ctrl-R or Cmd-R'],
     },
   ];
   
