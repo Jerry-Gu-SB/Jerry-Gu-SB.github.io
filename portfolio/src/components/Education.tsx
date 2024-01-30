3873dd00c015225f23cc136a40575d55d49a0199
Jerry Gu <ncq9fn@virginia.edu>
2024-01-29 23:12:34 -0500

    made text more responsive on education section

--- portfolio/src/components/Education.tsx
+++ portfolio/src/components/Education.tsx
@@ -7,23 +7,23 @@ const Education: React.FC = () => {
       <div className="flex flex-row w-full">
         <div className="flex flex-row w-1/2">
           <img src={require("./images/uva.png")} alt="University of Virginia" className="ml-5 mr-2 w-60 h-60" />
-          <div className="text-left text-3xl">
-            <h2 className="text-4xl font-bold">University of Virginia</h2>
-            <h1 className="text-2xl">Bachelor of Science in Computer Science</h1>
-            <ul className="list-disc list-inside">
-              <li className="text-xl">Studied computer science in the School of Engineering and Applied Sciences</li>
-              <li className="text-xl">Working towards a double major in philosophy</li>
-              <li className="text-xl">Currently holding a 3.82 GPA</li>
+          <div className="text-left">
+            <h2 className="text-xl sm:text-md md:text-xl lg:text-2xl xl:text-4xl font-bold">University of Virginia</h2>
+            <h1 className="text-sm sm:text-sm md:text-md lg:text-xl xl:text-2xl font-bold">Bachelor of Science in Computer Science</h1>
+            <ul className="list-disc list-inside text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+              <li>Studied computer science in the School of Engineering and Applied Sciences</li>
+              <li>Working towards a double major in philosophy</li>
+              <li>Currently holding a 3.82 GPA</li>
             </ul>
           </div>
         </div>
         <div className="flex flex-row w-1/2">
           <img src={require("./images/snu.png")} alt="Seoul National University" className="ml-5 mr-2 w-60 h-60" />
           <div className="text-left text-3xl">
-            <h2 className="text-4xl font-bold">Seoul National University</h2>
-            <h1 className="text-2xl">Study Abroad Spring 2024</h1>
-            <ul className="list-disc list-inside">
-              <li className="text-xl">Semester study in the Department of Computer Science and Engineering</li>
+            <h2 className="text-xl sm:text-md md:text-xl lg:text-2xl xl:text-4xl font-bold">Seoul National University</h2>
+            <h1 className="text-sm sm:text-sm md:text-md lg:text-xl xl:text-2xl font-bold">Study Abroad Spring 2024</h1>
+            <ul className="list-disc list-inside text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+              <li>Semester study in the Department of Computer Science and Engineering</li>
             </ul>
           </div>
         </div>
