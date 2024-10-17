630e6117b1bee70b21a78d08413ba5fda8a18458
Jerry Gu <ncq9fn@virginia.edu>
2024-10-16 21:42:13 -0400

    changed some text

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -113,7 +113,7 @@ const Home: React.FC = () => {
         <h1 className="text-4xl mb-2 ">Jerry Gu</h1>
         <p className="text-center text-2xl">Hey there! I'm a current student at the University of Virginia School of Engineering studying computer science looking for full time software developer positions. 
         I've created games, websites, and more so far in my software engineer journey and I'm always looking for opportunities to let me express myself in my work!
-        In my free time I love to create videos on YouTube and practice Kendo. I'm always looking for new things to learn and new people to meet, so feel free to use the contact form or links below to get in touch with me!
+        In my free time I love to create videos on YouTube and practice kendo. I'm always looking for new things to learn and new people to meet, so feel free to reach out to me and check out my stuff at the links below!
         </p>
       </div>
       <div className="mt-4 mb-4 flex space-x-2">
