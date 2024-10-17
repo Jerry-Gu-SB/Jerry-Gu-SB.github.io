59cb1b5e1dac2527d122f18ede97fcfefd0ab34e
Jerry Gu <ncq9fn@virginia.edu>
2024-10-16 21:40:21 -0400

    okay think this is a good stopping point

--- portfolio/src/components/ProjectList.tsx
+++ portfolio/src/components/ProjectList.tsx
@@ -2,32 +2,45 @@ import StudentCourseApp from './images/coursereview.jpg';
 import ShadowBox from './images/shadowbox.png';
 import SplitBall from './images/splitball.png';
 import Portfolio from './images/portolio.png';
-import HTML from './images/HTML.png';
+import HTTP from './images/HTTP.jpg';
 const ProjectList = [
     {
       title: 'Student Course Review App',
-      details: ['•	Designed and developed a class review application as a final project for Software Development Essentials course', '•	Engineered a 3-layer architecture pattern with JavaFX as the presentation layer, Java for logic, and SQLite for the backend'],
+      details: ['•	Designed and developed a class review application as a final project for Software Development Essentials course', 
+        '•	Engineered a 3-layer architecture pattern with JavaFX as the presentation layer, Java for logic, and SQLite for the backend'],
       image: StudentCourseApp,
+      link: '',
     },
     {
       title: 'ShadowBox Mouse Trainer ',
-      details: ['•	Conceived the original idea for a mouse coordination training game, identifying a need for such a tool', '•	Self-taught Unity and C# scripting to bring the game idea to life, successfully bringing new game to public ', '•	Play my game: jerrygu.itch.io/shadowbox'],
+      details: ['•	Conceived the original idea for a mouse coordination training game, identifying a need for such a tool', 
+        '•	Self-taught Unity and C# scripting to bring the game idea to life, successfully bringing new game to public ', 
+        '•	Try it out by clicking on this image!'],
       image: ShadowBox,
+      link: 'https://jerrygu.itch.io/shadowbox'
     },
     {
       title: 'SplitBall',
-      details: ['•	Inherited single player game as a team of two to repurpose as a multiplayer offline competitive game within the Unity Engine and C#', '•	Check out the repo: github.com/UVASGD/spring-2022-splitball'],
+      details: ['•	Inherited single player game as a team of two to repurpose as a multiplayer offline competitive game within the Unity Engine and C#', 
+        '•	Check out the repo by clicking on this image!'],
       image: SplitBall,
+      link: 'https://github.com/UVASGD/spring-2022-splitball',
     },
     {
       title: 'Personal Portfolio Website',
-      details: ['•	Constructed a personal portfolio website using React + TypeScript, and designed using Tailwind CSS', '•	Incorporated an email contact form using Formspree, providing direct communication with visitors', '•	Check out my website: Ctrl-R or Cmd-R'],
+      details: ['•	Constructed a personal portfolio website using React + TypeScript, and designed using Tailwind CSS', 
+        '•	Incorporated an email contact form using Formspree, providing direct communication with visitors', 
+        '•	Check out this website\'s source code by clicking on this image!'],
       image: Portfolio,
+      link: 'https://github.com/Jerry-Gu-SB/Jerry-Gu-SB.github.io'
     },
     {
-      title: 'HTML 1.0 File Server',
-      details: ['•	Utilized C programming language socket library to construct a file delivery webserver supporting subset of HTTP/1.0', '• Implemented functionality to handle GET and POST requests for efficient file transfer and data submission.'],
-      image: HTML,
+      title: 'HTTP 1.0 File Server',
+      details: ['•	Utilized C programming language socket library to construct a file delivery webserver supporting subset of HTTP/1.0', 
+        '• Implemented functionality to handle GET and POST requests for efficient file transfer and data submission',
+        '• Check out the repo by clicking here!'],
+      image: HTTP,
+      link: 'https://github.com/Jerry-Gu-SB/socket-server-client'
     }
   ];
   
--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ProjectList from "./ProjectList"; // Path to your projects file
+import ProjectList from "./ProjectList";
 
 const Projects: React.FC = () => {
   const settings = {
@@ -20,22 +20,32 @@ const Projects: React.FC = () => {
 
   return (
     <section id="Projects" className="p-10">
-      <h2 className="text-4xl mb-10 font-weight-bold">Projects</h2>
+      <h2 className="text-4xl mb-10 font-bold">Projects</h2>
       <Slider {...settings}>
         {ProjectList.map((project, index) => (
-          <div 
-            key={index} 
-            className="relative h-full p-4 hover:child:translate-y-0 hover:child:scale-110" 
-            onPointerEnter={() => setIsHovered(prevState => prevState.map((val, idx) => idx === index ? true : val))} 
+          <div
+            key={index}
+            className="relative h-full p-4 hover:child:translate-y-0 hover:child:scale-110"
+            onPointerEnter={() => setIsHovered(prevState => prevState.map((val, idx) => idx === index ? true : val))}
             onPointerLeave={() => setIsHovered(prevState => prevState.map((val, idx) => idx === index ? false : val))}
           >
-            <div
-              className={`h-full flex-grow rounded-lg transition-transform duration-300 transform-gpu ${isHovered[index] ? '-translate-y-full scale-110' : ''}`}
-            >
-              <img src={project.image} alt={project.title} className="rounded-lg w-full h-full object-cover" />
-            </div>
+            {project.link ? (
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                <div
+                  className={`h-full flex-grow rounded-lg transition-transform duration-300 transform-gpu ${isHovered[index] ? '-translate-y-full scale-110' : ''}`}
+                >
+                  <img src={project.image} alt={project.title} className="rounded-lg w-full h-full object-cover" />
+                </div>
+              </a>
+            ) : (
+              <div
+                className={`h-full flex-grow rounded-lg transition-transform duration-300 transform-gpu ${isHovered[index] ? '-translate-y-full scale-110' : ''}`}
+              >
+                <img src={project.image} alt={project.title} className="rounded-lg w-full h-full object-cover" />
+              </div>
+            )}
             <div className="z-[-10] absolute top-4 p-4 rounded-lg overflow-auto max-h-[calc(100%-8px)]">
-              <h3 className="text-sm sm:text-base md:text-md lg:text-l xl:text-2xl pt-4">{project.title}</h3>
+              <h3 className="text-sm sm:text-base md:text-md lg:text-l xl:text-2xl pt-4 font-bold">{project.title}</h3>
               <ul className="text-sm sm:text-base md:text-sm lg:text-md xl:text-xl mt-2 space-y-2">
                 {project.details.map((detail, i) => (
                   <li key={i}>{detail}</li>
