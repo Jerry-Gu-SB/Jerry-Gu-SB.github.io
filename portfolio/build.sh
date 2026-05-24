fa58864a80a9a87bc9d329ccc45b692a360239ce
Jerry Gu <jerrysonggu@gmail.com>
2026-05-24 18:33:52 -0400

    STATIC SITE: INIT

---
+++ portfolio/build.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+rm -r cmake-build-debug
+mkdir -p cmake-build-debug && cd cmake-build-debug
+cmake ..
+make
+./StaticSiteGenerator
---
+++ portfolio/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <filesystem>
+#include <iostream>
+// #include "maddy/parser.h"
+
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+
+
+
+int main() {
+
+    const auto MD_FILE_DIRECTORY = "yipppeee";
+
+    if (!std::filesystem::exists(MD_FILE_DIRECTORY)) {
+        std::cout << MD_FILE_DIRECTORY << " does not exist!\n" << std::endl;
+        std::filesystem::create_directory(MD_FILE_DIRECTORY);
+    }
+
+    for (const auto& entry : std::filesystem::directory_iterator(MD_FILE_DIRECTORY)) {
+
+    }
+
+}
