d66a2ae3dc4e003d765a9b090d263ac56b55c234
Jerry Gu <jerrysonggu@gmail.com>
2026-08-04 00:03:46 -0400

    Can now generate html pages recursively through md parsing. Now need a way to propagate down the CSS to also apply recursively

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,91 @@
 #include <md4c.h>
 #include <md4c-html.h>
 
+
+const std::string MD_FILE_DIRECTORY = "../md_files";
+const std::string PUBLIC_DIRECTORY = "../../docs";
+
+
 void html_output(const MD_CHAR* text, MD_SIZE size, void* userdata)
 {
-    std::string* out = static_cast<std::string*>(userdata);
+    auto* out = static_cast<std::string*>(userdata);
     out->append(text, size);
 }
 
-int main() {
+void create_directory(const std::string& directory_string) {
+    if (!std::filesystem::exists(directory_string)) {
+        std::cout << directory_string << " does not exist!\n" << std::endl;
+        std::filesystem::create_directory(directory_string);
+    }
+}
 
-    const std::string MD_FILE_DIRECTORY = "../md_files";
-    const std::string PUBLIC_DIRECTORY = "../../docs";
-
-
-    if (!std::filesystem::exists(MD_FILE_DIRECTORY)) {
-        std::cout << MD_FILE_DIRECTORY << " does not exist!\n" << std::endl;
-        std::filesystem::create_directory(MD_FILE_DIRECTORY);
+bool write_html_file(const std::string& htmlOutput, const std::string& destination) {
+    std::ofstream html_file(destination, std::ios::binary);
+    if (!html_file.is_open()) {
+        std::cout << "ERROR: Failed to open: " << destination << std::endl;
+        return true;
     }
 
-    if (!std::filesystem::exists(PUBLIC_DIRECTORY)) {
-        std::cout << PUBLIC_DIRECTORY << " does not exist!\n" << std::endl;
-        std::filesystem::create_directory(PUBLIC_DIRECTORY);
-    }
+    const std::string header_html = R"(<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="styles.css">
+  <meta charset="utf-8" />
+  <title>Under Construction...</title>
+  <link rel="icon" type="image/x-icon" href="/artifacts/favicon.png">
+</head>
+<body>
+<div id="content-wrapper">
+        )";
 
-    for (const auto& entry : std::filesystem::directory_iterator(MD_FILE_DIRECTORY)) {
+    html_file << header_html;
+    html_file << htmlOutput;
+
+    const std::string footer_html = R"(
+</div>
+</body>
+</html>
+        )";
+
+    html_file << footer_html << std::endl;
+    html_file.close();
+    return false;
+}
+
+// By Tyler McHenry @ https://stackoverflow.com/a/2602060
+std::string get_markdown_raw(const std::string& markdown_path) {
+    std::ifstream t(markdown_path);
+    std::string str;
+
+    t.seekg(0, std::ios::end);
+    str.reserve(t.tellg());
+    t.seekg(0, std::ios::beg);
+
+    str.assign((std::istreambuf_iterator<char>(t)),
+               std::istreambuf_iterator<char>());
+    return str;
+}
+
+bool build_html_in_directory(const std::string& directory_path) {
+    for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
         std::string markdown_path(entry.path().string());
         std::cout << "Current file: " << markdown_path << std::endl;
 
-        // By Tyler McHenry @ https://stackoverflow.com/a/2602060
-        std::ifstream t(markdown_path);
-        std::string str;
+        if (entry.is_directory()) {
 
-        t.seekg(0, std::ios::end);
-        str.reserve(t.tellg());
-        t.seekg(0, std::ios::beg);
+            const std::string entry_string = entry.path().string();
+            const std::size_t folder_index = entry.path().string().find(MD_FILE_DIRECTORY);
+            const std::string new_folder_path = entry_string.substr(folder_index + MD_FILE_DIRECTORY.length());
+            const std::string new_folder_destination = PUBLIC_DIRECTORY + new_folder_path;
 
-        str.assign((std::istreambuf_iterator<char>(t)),
-                    std::istreambuf_iterator<char>());
+            create_directory(new_folder_destination);
+            build_html_in_directory(markdown_path);
+            return false;
+        }
 
+        std::string str = get_markdown_raw(markdown_path);
 
         std::string htmlOutput;
-        std::ostringstream htmlOutputString;
         int result = md_html(
             str.c_str(),
             str.size(),
@@ -55,45 +100,37 @@ int main() {
 
         if (result != 0) {
             std::cout << "ERROR: md_html returned status 1" << std::endl;
-            return 1;
+            return true;
         }
 
-        std::string file_name = markdown_path.erase(0, MD_FILE_DIRECTORY.length() + 1);
+        // File Cleaning
+        std::string file_name;
+        file_name = markdown_path.erase(0, directory_path.length() + 1);
         size_t dot_location = file_name.find('.');
         file_name = file_name.substr(0, dot_location);
-        std::cout << "Cleaned file name: " << file_name << std::endl;
         file_name.append(".html");
 
-        std::string destination = PUBLIC_DIRECTORY;
+        // Destination path building
+        std::size_t md_directory_index = directory_path.find(MD_FILE_DIRECTORY);
+        std::string folder_path = directory_path.substr(md_directory_index + MD_FILE_DIRECTORY.length());
+        std::string destination = PUBLIC_DIRECTORY + folder_path;
         destination.append("/").append(file_name);
-        std::cout << destination << std::endl;
 
-        std::ofstream html_file(destination, std::ios::binary);
-        if (!html_file.is_open()) {
-            std::cout << "ERROR: Failed to open: " << destination << std::endl;
-            return 1;
+        std::cout << "Writing to: " << destination << std::endl;
+
+        if (write_html_file(htmlOutput, destination)) {
+            return true;
         }
-        std::string header_html = R"(<!DOCTYPE html>
-<html lang="en">
-<head>
-  <link rel="stylesheet" href="styles.css">
-  <meta charset="utf-8" />
-  <title>Under Construction...</title>
-  <link rel="icon" type="image/x-icon" href="/artifacts/favicon.png">
-</head>
-<body>
-<div id="content-wrapper">
-        )";
-        html_file << header_html;
-        html_file << htmlOutput;
+    }
+    return false;
+}
 
-        std::string footer_html = R"(
-</div>
-</body>
-</html>
-        )";
-        html_file << footer_html << std::endl;
-        html_file.close();
+int main() {
+    create_directory(MD_FILE_DIRECTORY);
+    create_directory(PUBLIC_DIRECTORY);
+
+    if (build_html_in_directory(MD_FILE_DIRECTORY)) {
+        return 1;
     }
     std::cout << "done!" << std::endl;
     return 0;
